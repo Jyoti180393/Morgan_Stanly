270c746a09677209e50c87b81658c2d6b75053f7
Jyoti180393 <pokhariajyoti18@gmail.com>
2021-10-21 17:06:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-gdnmjn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-gdnmjn)
---
+++ script.js
@@ -0,0 +1,47 @@
+let arr = [5, 7, 10, 8, 9];
+let arr1 = [2, 10, 5, 6, 9];
+
+// Array.prototype.forEach = function () {
+//   console.log('for each');
+// };
+// console.log(arr.forEach);
+
+function unique(a, b) {
+  let res = [];
+  a.map((val) => {
+    if (b.indexOf(val) === -1) {
+      res.push(val);
+    }
+  });
+  b.map((val) => {
+    if (a.indexOf(val) === -1) {
+      res.push(val);
+    }
+  });
+  // let arr2 = a.concat(b);
+  // let res = new [...Set(arr2)];
+  console.log(res);
+}
+
+// unique(arr, arr1);
+
+var a = 10,
+  b = 20,
+  c = 30;
+
+function print() {
+  b = 25;
+  c = 35;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+  var a = 15;
+  d = 50;
+}
+
+print();
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
